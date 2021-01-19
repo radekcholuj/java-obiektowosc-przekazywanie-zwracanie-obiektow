b0e8890ffe69fe5e8bedb6a526e0d4d5874b662c
Radoslaw Choluj <radekcholuj@wp.pl>
2021-01-19 19:21:06 +0100

    #8 dodanie wyszukiwania lotu po numerze lotu

---
+++ src/Plane.java
@@ -0,0 +1,11 @@
+public class Plane {
+    int id;
+
+    public Plane(int planeID) {
+        this.id = planeID;
+    }
+        void displayPlaneInfo () {
+            System.out.println(id);
+        }
+    }
+
